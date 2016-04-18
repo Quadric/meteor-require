9daf1cd7a6bf7b7589f4758dc55d15a0019459ea
zVictor <victor0110@msn.com>
2016-04-18 12:37:54 +0200

    isolate babel-require package

--- packages/babel-cli/test/fixtures/babel/dir --out-dir --source-maps inline/out-files/lib/foo.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-
-arr.map(function (x) {
-  return x * MULTIPLIER;
-});
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uL3NyYy9mb28uanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7QUFBQSxJQUFJLEdBQUosQ0FBUTtTQUFLLElBQUksVUFBSjtDQUFMLENBQVIiLCJmaWxlIjoiZm9vLmpzIiwic291cmNlc0NvbnRlbnQiOlsiYXJyLm1hcCh4ID0+IHggKiBNVUxUSVBMSUVSKTsiXX0=
